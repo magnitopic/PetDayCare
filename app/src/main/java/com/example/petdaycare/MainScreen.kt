e8b5a3365f0f83755edda4955f7f29ed5aa0af85
magnitopic <magnitopic@gmail.com>
2023-05-01 21:47:21 +0200

    added mainScreen

--- app/src/main/java/com/example/petdaycare/MainScreen.kt
+++ app/src/main/java/com/example/petdaycare/MainScreen.kt
@@ -5,13 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.loginButton).setOnClickListener {
-            var i = Intent(this, MainScreen::class.java)
-            startActivity(i)
-        }
+        setContentView(R.layout.activity_main_screen)
+
     }
 }
