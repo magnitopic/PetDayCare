0331c50d22f1f7ede41f360a6f4145dc089105ac
magnitopic <magnitopic@gmail.com>
2023-05-08 11:47:54 +0200

    New screen and navegation

--- app/src/main/java/com/example/petdaycare/MainScreen.kt
+++ app/src/main/java/com/example/petdaycare/MainScreen.kt
@@ -4,11 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ListView
+import android.widget.TextView
 
 class MainScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_screen)
-
+        val empty = findViewById<TextView>(R.id.empty)
+        val petList = findViewById<ListView>(R.id.petList)
+        petList.emptyView = empty
+        findViewById<ImageButton>(R.id.newPetButton).setOnClickListener {
+            var i = Intent(this, NewPet::class.java)
+            startActivity(i)
+        }
     }
 }
---
+++ app/src/main/java/com/example/petdaycare/NewPet.kt
@@ -0,0 +1,11 @@
+package com.example.petdaycare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewPet : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_pet)
+    }
+}
