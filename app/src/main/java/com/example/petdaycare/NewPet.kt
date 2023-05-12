2029b01c0ddec99dec2734acb90c81ef0aef95ce
magnitopic <magnitopic@gmail.com>
2023-05-12 14:06:36 +0200

    finished main design for application

--- app/src/main/java/com/example/petdaycare/NewPet.kt
+++ app/src/main/java/com/example/petdaycare/NewPet.kt
@@ -2,10 +2,21 @@ package com.example.petdaycare
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class NewPet : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_pet)
+        val spinner: Spinner = findViewById(R.id.spinner)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.generos,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
     }
 }
