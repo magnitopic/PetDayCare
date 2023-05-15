8e7687c8debe24006d03aba50edb30182e445387
magnitopic <magnitopic@gmail.com>
2023-05-15 11:50:35 +0200

    beginnigs of kotlin, some redesigns

---
+++ app/src/main/java/com/example/petdaycare/Pet.kt
@@ -0,0 +1,3 @@
+package com.example.petdaycare
+
+data class Pet(val nombre: String,  val raza: String, val sexo: String, val peso: Int)
---
+++ app/src/main/java/com/example/petdaycare/PetArrayAddapter.kt
@@ -0,0 +1,10 @@
+package com.example.petdaycare
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class PetArrayAdapter(context: Context, viewToPaint: Int, private val petList: ArrayList<Pet>) {
+
+}
